50ba28f4d05fb034e77d5e7afe33a4a84ad2103a
Siddhanth B N <56065640+pol905@users.noreply.github.com>
2021-08-09 00:13:14 +0530

    Fix: Broken factory contract update

--- ethereum/campaignGenerator.js
+++ ethereum/campaignGenerator.js
@@ -1,11 +1,8 @@
-import web3 from './web3'
-import CampaignGenerator from './build/CampaignGenerator.json'
+import web3 from "./web3";
+import CampaignGenerator from "./build/CampaignGenerator.json";
 
-const factoryAddress = '0x5556DE07BbD71D4cb16047aeE0c87f1Df6A1636b'
+const factoryAddress = "0x4B532eaC7A15DBA6996Ad4FDf9Ce73e3ad04c591";
 
-const factory = new web3.eth.Contract(
-    CampaignGenerator.abi,
-    factoryAddress
-    );
+const factory = new web3.eth.Contract(CampaignGenerator.abi, factoryAddress);
 
-export default factory
+export default factory;
